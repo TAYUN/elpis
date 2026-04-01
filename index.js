1931b48ba3490fe901206ed4b2efd21553100d1c
tangyy <plmqaz0.>
2026-04-01 15:13:36 +0800

    refactor: 重构入口文件，使用Elpis核心模块启动服务

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const Koa = require('koa')
+const Elpis = require('./elpis-core');
 
-// 创建koa实例
-const app = new Koa()
-// 启动服务
-
-try {
-    console.log(process.env)
-    const port = process.env.PORT || 8080
-    const host = process.env.IP || '0.0.0.0'
-    app.listen(port, host)
-    console.log(`Server is running at http://${host}:${port}`)
-} catch (error) {
-    console.error(error)
-    
-}
+Elpis.start();
